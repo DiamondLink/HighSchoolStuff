51db650699cd3008ee8d8b21aba55321f3333a08
DiamondLink <59293428+DiamondLink@users.noreply.github.com>
2020-04-25 13:18:09 +0200

    Add files via upload

--- NSI/baptiste_villeneuve_mes_fonctions.py
+++ NSI/baptiste_villeneuve_mes_fonctions.py
@@ -1,50 +1,58 @@
+#encode : utf - 8
 import os
 import csv
-from tkinter import filedialog,Tk
-os.chdir("C://Users//BAPTISTE//Downloads")
+from tkinter import filedialog, Tk
 
+#a)
 def lire_csv_dictionnaire(nomFichier):
-    csvfile =open(nomFichier,"r")
-    contenu=csv.DictReader(csvfile, delimiter=';')
-    table=[]
-    for ligne in contenu :
+    csvfile = open(nomFichier, "r")
+    contenu = csv.DictReader(csvfile, delimiter=';')
+    table = []
+    for ligne in contenu:
         table.append(ligne)
     csvfile.close()
     return(table)
 
+#b)
 def lire_csv_list(nomFichier):
     liste = list()
-    with open(nomFichier,"r") as fichier:
+    with open(nomFichier, "r") as fichier:
         for ligne in fichier:
             liste.append(ligne.rstrip("\n").split(";"))
     return liste
 
-def ecrire_Dans_CSV_dictionnaire(nomFichierCSV,listeDict): 
+#c)
+def ecrire_Dans_CSV_dictionnaire(nomFichierCSV, listeDict):
     with open(nomFichierCSV, 'w', newline="") as csv_file:
-        enTete=[cle for cle in listeDict[0].keys()] 
-        contenant = csv.DictWriter(csv_file, fieldnames=enTete, delimiter=';') 
+        enTete = [cle for cle in listeDict[0].keys()]
+        contenant = csv.DictWriter(csv_file, fieldnames=enTete, delimiter=';')
 
         contenant.writeheader()
         for ligne in listeDict:
+            if None in ligne.keys():
+                del(ligne[None])
             contenant.writerow(ligne)
 
-def ecrire_Dans_CSV_list(nomFichierCSV,listeList):
+#d)
+def ecrire_Dans_CSV_list(nomFichierCSV, listeList):
     with open(nomFichierCSV, 'w', newline="") as csv_file:
         for ligne in listeList:
             csv_file.write(";".join(ligne) + "\n")
 
-
-def rechercheFichier(titre, fileTypes : list, rep=os.getcwd()):
-    fic=""
-    root=Tk()
+#e)
+def rechercheFichier(titre, fileTypes: list, rep=os.getcwd()):
+    fic = ""
+    root = Tk()
     root.withdraw()
-    nomfichier=filedialog.askopenfilename(title =titre,initialdir =rep,
+    nomfichier = filedialog.askopenfilename(title=titre, initialdir=rep,
     initialfile=fic, filetypes=fileTypes)
     root.destroy()
-    return(nomfichier) 
+    return(nomfichier)
 
-def remplacer_string_par_int(nomFichier, colonnesAConvertir : list = [1,2]):
-    with open (nomFichier,"r") as fichier:
+
+#f)
+def remplacer_string_par_int(nomFichier, colonnesAConvertir: list = [1, 2]):
+    with open(nomFichier, "r") as fichier:
         fichierConverti = None
         for ligne in fichier:
             ligne = ligne.rstrip("\n")
@@ -57,37 +65,35 @@ def remplacer_string_par_int(nomFichier, colonnesAConvertir : list = [1,2]):
                     for colonneIndex in colonnesAConvertir:
                         if listeDeLignes[colonneIndex] == colonne:
                             for element in colonne:
-                                if element == " ":
-                                    fichierConverti += element
+                                try:
+                                    element = int(element)
+                                    fichierConverti += str(element)
                                     isChanged = True
-                                else:
-                                    try:
-                                        element = int(element)
-                                        fichierConverti += str(element)
-                                        isChanged = True
-                                    except:
-                                        pass
+                                except:
+                                    pass
 
                     if not isChanged:
                         fichierConverti += colonne
                     fichierConverti += ";"
                 fichierConverti += "\n"
-    
-    with open(nomFichier,"w") as fichier:
+
+    with open(nomFichier, "w") as fichier:
         fichier.write(fichierConverti)
 
+#g)
 def supprimerDoublons(nomFichier):
     fichierSansDoublons = ""
-    with open(nomFichier,"r") as fichier:
+    with open(nomFichier, "r") as fichier:
         for ligne in fichier:
             if not ligne in fichierSansDoublons:
                 fichierSansDoublons += ligne
 
-    with open(nomFichier,"w") as fichier:
+    with open(nomFichier, "w") as fichier:
         fichier.write(fichierSansDoublons)
 
-def trierColonne(nomFichier,colonneATrier : int, croissant = True, inclureLaPremiereLigne = True):
-    with open(nomFichier,"r") as fichier:
+#h)
+def trierColonne(nomFichier, colonneATrier: int, croissant=True, inclureLaPremiereLigne=True):
+    with open(nomFichier, "r") as fichier:
         listeDeLignes = fichier.readlines()
         if inclureLaPremiereLigne:
             premiereLigne = listeDeLignes[0]
@@ -96,8 +102,9 @@ def trierColonne(nomFichier,colonneATrier : int, croissant = True, inclureLaPrem
         listeDeLigneSplite = list()
 
         for ligne in listeDeLignes:
-            listeDeLigneSplite.append(ligne.rstrip("\n").split(";")[colonneATrier])
-        
+            listeDeLigneSplite.append(
+                ligne.rstrip("\n").split(";")[colonneATrier])
+
         for i in range(len(listeDeLigneSplite)):
             colonneTemporaire = ""
             for element in listeDeLigneSplite[i]:
@@ -108,7 +115,8 @@ def trierColonne(nomFichier,colonneATrier : int, croissant = True, inclureLaPrem
                     pass
             listeDeLigneSplite[i] = colonneTemporaire
 
-        ordreDesLigne = sorted(range(len(listeDeLigneSplite)), key=lambda k: int(listeDeLigneSplite[k]))
+        ordreDesLigne = sorted(
+            range(len(listeDeLigneSplite)), key=lambda k: int(listeDeLigneSplite[k]))
 
         listeDeLignesTrie = list()
         for index in ordreDesLigne:
@@ -116,18 +124,80 @@ def trierColonne(nomFichier,colonneATrier : int, croissant = True, inclureLaPrem
 
         if not croissant:
             listeDeLignesTrie.reverse()
-        
+
         if inclureLaPremiereLigne:
             listeDeLignesTrie = premiereLigne.split() + listeDeLignesTrie
-    
-    with open(nomFichier,"w") as fichier:
+
+    with open(nomFichier, "w") as fichier:
         for i in range(len(listeDeLignesTrie)):
             if listeDeLignesTrie[i][-1:] != "\n":
                 listeDeLignesTrie[i] += "\n"
         fichier.write("".join(listeDeLignesTrie))
 
-trierColonne("tableau.csv",colonneATrier=1)
+#i)
+def supprimerColonne(nomFichier, colonneASupprimer: int):
+    fichierAvecLaColonneEnMoins = ""
+    with open(nomFichier,"r") as fichier:
+        listeDeLignes = fichier.readlines()
+        for ligne in listeDeLignes:
+            ligne = ligne.rstrip("\n")
+            ligne = ligne.split(";")
+            del(ligne[colonneASupprimer])
+            fichierAvecLaColonneEnMoins += ";".join(ligne) + "\n"
+            
+    with open(nomFichier,"w") as fichier:
+        fichier.write(fichierAvecLaColonneEnMoins)
 
+#j)
+def ajoutDensite(nomFichier):
+    remplacer_string_par_int(nomFichier,colonnesAConvertir=[1,2])
+    listeDictionnaire = lire_csv_dictionnaire(nomFichier)
+    for i in range(len(listeDictionnaire)):
+        listeDictionnaire[i]["Densité"] = str(int(listeDictionnaire[i]["Population"]) // int(listeDictionnaire[i]["Superficie"]))
+    
+    ecrire_Dans_CSV_dictionnaire(nomFichier,listeDictionnaire)
 
+#k)
+def fusionDeDeuxTablesAyantuneCleeCommune(table1 : list,table2 : list,cleCommune):
+    for dico1 in table1:
+        for dico2 in table2:
+            if dico1[cleCommune] == dico2[cleCommune]:
+                for key in dico2.keys():
+                    if key != cleCommune:
+                        dico1[key] = dico2[key]
 
+    return table1
 
+#l)
+def selecLigneDict(listeDict,cle,signe,critere):
+    listDict2 = []
+    for dico in listeDict:
+        try:
+            if signe == ">":
+                if dico[cle] > critere:
+                    listDict2.append(dico["Pays"])
+            elif signe == "<":
+                if dico[cle] < critere:
+                    listDict2.append(dico["Pays"])
+            elif signe == "!=":
+                if dico[cle] != critere:
+                    listDict2.append(dico["Pays"])
+            elif signe == "=":
+                if dico[cle] == critere:
+                    listDict2.append(dico["Pays"])
+        except:
+            pass
+
+    return listDict2
+assert selecLigneDict([{"Pays" : "France", "population" : 65},{"Pays" : "Allemagne", "population" : 85},{"Pays" : "Italie","population" : 60}], "population","<",70) == ["France","Italie"]
+
+#k)
+if __name__ == "__main__": 
+    listePays = lire_csv_dictionnaire("Monde_Nbre_Habitants_2019_Superficie.csv")
+    listeMonaie = lire_csv_dictionnaire("Monde_Monnaie.csv")
+    listeCapitale = lire_csv_dictionnaire("Monde_Capitale_Continent.csv")
+
+    paysETmonaieFusionne = fusionDeDeuxTablesAyantuneCleeCommune(listePays, listeMonaie, "Pays")
+    tablesFusionne = fusionDeDeuxTablesAyantuneCleeCommune(paysETmonaieFusionne, listeCapitale,"Pays")
+
+    ecrire_Dans_CSV_dictionnaire("Monde_Nbre_Habitants_2019_Superficie.csv",tablesFusionne)
